a90a02544ff8f1686c150196ef484875985ec82d
seangarang <seangarang01@gmail.com>
2023-12-15 10:59:23 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
+#include  <stdio.h>
 /**
- * main - Aprogram that prints a line with puts function 
+ * main - Aprogram that prints a line with puts function
  * Return: 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+return (
