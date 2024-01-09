9ecddccd64c4b91e4a68da7793e09a8edb9fca30
seangarang <seangarang01@gmail.com>
2024-01-09 03:05:59 -0800

    task

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 char *_memset(char *s, char b, unsigned int n);
-int_putchar(char c);
+int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
@@ -12,3 +12,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
+#endif
